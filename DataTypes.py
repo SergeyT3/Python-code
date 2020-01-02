39df69b8687af0c4dad2db5554a26ad4dc4ec2af
Sergey <37253304+SergeyT3@users.noreply.github.com>
2020-01-02 15:11:04 -0800

    Third commit

---
+++ DataTypes.py
@@ -0,0 +1,13 @@
+#Number --> int float
+
+x=100    #int type
+y=10.5   #float type
+s="welcome"   # string (str)/char type
+b=True        # boolean (bool) Type (True or False)
+
+# to know the type of variable
+
+print (type(x))   #you will get <class 'int'>
+print(type(y))    #you will get <class 'float'>
+print(type(s))    #you will get <class 'str'>
+print(type(b))    #you will get <class 'bool'>
